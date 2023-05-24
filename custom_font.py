24111f55a4810d7dbd980fcea1043ba8d22f3661
Kieran Farr <kieran.farr@gmail.com>
2023-05-24 16:09:01 -0700

    new cut of custom font demo

--- custom_font.py
+++ custom_font.py
@@ -3,46 +3,20 @@ import time
 import math
 from cosmic import CosmicUnicorn
 from picographics import PicoGraphics, DISPLAY_COSMIC_UNICORN as DISPLAY
-from bitmap_fonts import font2x5, font3x5, font4x5, font5x9
+from bitfonts import BitFont, font2x5, font3x5, font4x5, font5x9
 
 # INITIALISE GALACTIC UNICORN
 cu = CosmicUnicorn()
 graphics = PicoGraphics(DISPLAY)
+bitfont = BitFont(graphics)
 
 width = CosmicUnicorn.WIDTH
 height = CosmicUnicorn.HEIGHT
 
-
-cu.set_brightness(0.3)
-
-# DRAW A SINGLE BITMAP FONT CHARACTER
-@micropython.native  # noqa: F821
-def draw_char(d,x,y,f):
-    # LOOP THROUGH ROWS
-    for i in range(f[d]["h"]):
-        # LOOP THROUGH COLUMNS
-        for j in range(f[d]["w"]):
-            # IF THIS BIT IS SET THEN DRAW A PIXEL
-            if f[d]["data"] & (0b1 << ((i*f[d]["w"])+j)):
-                graphics.pixel(f[d]["w"]-1-j+x,f[d]["h"]-1-i+y)
-
-# DRAW A STRING WITH BITMAP FONT
-@micropython.native  # noqa: F821
-def draw_text(s,x,y,f,d=1):
-    
-    # LEFT JUSTIFIED
-    if d == 1:
-        for i in range(len(s)):
-            draw_char(s[i],x,y,f)
-            x += f[s[i]]["w"] + f[s[i]]["s"]
-    else:
-    # RIGHT JUSTIFIED
-        for i in reversed(range(len(s))):
-            x -= f[s[i]]["w"]
-            draw_char(s[i],x,y,f)
-            x -= f[s[i]]["s"]
+cu.set_brightness(1)
 
 paused = 0
+TEST_TEXT = "1234567890"
 
 # MAIN LOOP
 while True:
@@ -58,16 +32,19 @@ while True:
     graphics.set_pen(graphics.create_pen(0, 0, 0))
     graphics.clear()
 
-    text = "✋🚶"
-    timer_text = str(time_ms)[3:]
+#    text = "✋🚶"
+#    timer_text = str(time_ms)[3:]
+    text = "BITFONTS"
     if paused:
-        timer_text = "0123456789"
+        text = "PICOFONTS"
 
     if cu.is_pressed(CosmicUnicorn.SWITCH_A):
         text = "A"
+        paused = not paused
 
     if cu.is_pressed(CosmicUnicorn.SWITCH_B):
         text = "B"
+        paused = not paused
 
     if cu.is_pressed(CosmicUnicorn.SWITCH_C):
         text = "Play"
@@ -93,25 +70,23 @@ while True:
         text = "Zzz... zzz..."
 
     graphics.set_pen(graphics.create_pen(255, 255, 255))
-    draw_text(timer_text,0,0,font2x5)
 
-    draw_text(timer_text,0,6,font3x5)
-
-    draw_text(timer_text,0,12,font4x5)
-
-    draw_text(timer_text,0,16,font5x9)
-
-    draw_text(text.upper(),6,26,font3x5)
-
-    if paused:
-        graphics.set_pen(graphics.create_pen(255, 0, 0))
-        draw_char("-",(time_ms // 1000) % 2,21,font3x5)
+    if not paused:
+        bitfont.draw_text(TEST_TEXT,0,0,font2x5)
+        bitfont.draw_text(TEST_TEXT,0,6,font3x5)
+        bitfont.draw_text(TEST_TEXT,0,12,font4x5)
+        bitfont.draw_text(TEST_TEXT,0,16,font5x9)
+        bitfont.draw_text(text.upper(),0,26,font3x5)
     else:
-        graphics.set_pen(graphics.create_pen(0, 255, 0))
-        draw_char("-",(time_ms // 1000) % 2,23,font3x5)
-
+        graphics.set_font('bitmap6')
+        graphics.text(TEST_TEXT,0,-1,scale=1)
+        graphics.set_font('bitmap8')
+        graphics.text(TEST_TEXT,0,6,scale=1)
+        graphics.set_font('bitmap14_outline')
+        graphics.text(TEST_TEXT,0,12,scale=1)
+        graphics.set_font('bitmap6')
+        graphics.text(text.upper(),0,26,scale=1)
+    
     cu.update(graphics)
 
-    # pause for a moment
-#     time.sleep(0.1)
-
+    time.sleep(0.1)
