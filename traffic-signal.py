0d3762f679e44970c70b21df861253facd389d12
Kieran Farr <kieran.farr@gmail.com>
2023-06-07 11:58:04 -0700

    update signals to support new bitfont class

--- traffic-signal.py
+++ traffic-signal.py
@@ -3,43 +3,17 @@ import time
 import math
 from cosmic import CosmicUnicorn
 from picographics import PicoGraphics, DISPLAY_COSMIC_UNICORN as DISPLAY
-from bitmap_fonts import font2x5, font3x5, font4x5, font5x9
+from bitfonts import BitFont, font2x5, font3x5, font4x5, font5x9
 
 # INITIALISE GALACTIC UNICORN
 cu = CosmicUnicorn()
 graphics = PicoGraphics(DISPLAY)
+bitfont = BitFont(graphics)
 
 width = CosmicUnicorn.WIDTH
 height = CosmicUnicorn.HEIGHT
 
 cu.set_brightness(0.8)
-
-# DRAW A SINGLE BITMAP FONT CHARACTER
-@micropython.native  # noqa: F821
-def draw_char(d,x,y,f):
-    # LOOP THROUGH ROWS
-    for i in range(f[d]["h"]):
-        # LOOP THROUGH COLUMNS
-        for j in range(f[d]["w"]):
-            # IF THIS BIT IS SET THEN DRAW A PIXEL
-            if f[d]["data"] & (0b1 << ((i*f[d]["w"])+j)):
-                graphics.pixel(f[d]["w"]-1-j+x,f[d]["h"]-1-i+y)
-
-# DRAW A STRING WITH BITMAP FONT
-@micropython.native  # noqa: F821
-def draw_text(s,x,y,f,d=1):
-    
-    # LEFT JUSTIFIED
-    if d == 1:
-        for i in range(len(s)):
-            draw_char(s[i],x,y,f)
-            x += f[s[i]]["w"] + f[s[i]]["s"]
-    else:
-    # RIGHT JUSTIFIED
-        for i in reversed(range(len(s))):
-            x -= f[s[i]]["w"]
-            draw_char(s[i],x,y,f)
-            x -= f[s[i]]["s"]
 
 # SIGNAL HEAD FUNCTION
 def draw_signal_head(x, y, lamp="none"):
@@ -62,12 +36,12 @@ def draw_signal_head(x, y, lamp="none"):
 def draw_signal_row(n, y, state, state_ped):
     # ROW LABEL
     graphics.set_pen(graphics.create_pen(100, 100, 100))
-    draw_char(str(n), 0, y, font3x5)
+    bitfont.draw_char(str(n), 0, y, font3x5)
 
     # PED COUNTER BG
     if state_ped:
         graphics.set_pen(graphics.create_pen(15, 0, 0))
-        draw_text("00", 10, y, font2x5)
+        bitfont.draw_text("00", 10, y, font2x5)
     
     # SIGNALS MAST
     graphics.set_pen(graphics.create_pen(50, 50, 50))
@@ -84,14 +58,14 @@ def draw_signal_row(n, y, state, state_ped):
     # PED SYMBOL
     if state_ped == "W":
         graphics.set_pen(graphics.create_pen(255, 255, 255))
-        draw_char("🚶", 5, y, font3x5)
+        bitfont.draw_char("🚶", 5, y, font3x5)
     if state_ped == "R":
         graphics.set_pen(graphics.create_pen(255, 0, 0))
-        draw_char("✋", 5, y, font3x5)
+        bitfont.draw_char("✋", 5, y, font3x5)
     if state_ped == "F":
         invert_flash = 1 - (time_ms // 500) % 2
         graphics.set_pen(graphics.create_pen(255 * invert_flash, 0, 0))
-        draw_char("✋", 5, y, font3x5)
+        bitfont.draw_char("✋", 5, y, font3x5)
 
 def ped_timing_count_Fs(s):
     start_index = s.find('F')
@@ -172,12 +146,12 @@ while True:
         # PED COUNTERS
         graphics.set_pen(graphics.create_pen(255, 0, 0))
         count, start_index, end_index = ped_timing_count_Fs(phase2p_timing)
-        draw_text(ped_timer_string(count, start_index, end_index, cycle_seconds),15,1,font2x5,-1)
+        bitfont.draw_text(ped_timer_string(count, start_index, end_index, cycle_seconds),15,1,font2x5,-1)
         count4, start_index4, end_index4 = ped_timing_count_Fs(phase4p_timing)
-        draw_text(ped_timer_string(count4, start_index4, end_index4, cycle_seconds),15,26,font2x5,-1)
+        bitfont.draw_text(ped_timer_string(count4, start_index4, end_index4, cycle_seconds),15,26,font2x5,-1)
         # GLOBAL TIMER
         graphics.set_pen(graphics.create_pen(100, 100, 100))
-        draw_text(str(cycle_seconds),32,13,font3x5,-1)
+        bitfont.draw_text(str(cycle_seconds),32,13,font3x5,-1)
     if scene == 'B':
         draw_signal_row(' ', 26, phase2_timing[cycle_seconds],'')
         # RED
@@ -197,7 +171,7 @@ while True:
         graphics.circle(6, 25, 6)
         # GLOBAL TIMER
         graphics.set_pen(graphics.create_pen(50, 50, 50))
-        draw_text(str(cycle_seconds),32,27,font3x5,-1)
+        bitfont.draw_text(str(cycle_seconds),32,27,font3x5,-1)
     if scene == 'C':
         draw_signal_row(' ', 26, phase2_timing[cycle_seconds],'')
         # RED
@@ -212,7 +186,7 @@ while True:
         graphics.circle(15, 31, 10)
         # GLOBAL TIMER
         graphics.set_pen(graphics.create_pen(50, 50, 50))
-        draw_text(str(cycle_seconds),32,27,font3x5,-1)
+        bitfont.draw_text(str(cycle_seconds),32,27,font3x5,-1)
 
     if scene == 'D':
         # YELLOW
@@ -227,9 +201,9 @@ while True:
         graphics.circle(15, 21, 10)
         # GLOBAL TIMER
         graphics.set_pen(graphics.create_pen(50, 50, 50))
-        draw_text(str(cycle_seconds),32,27,font3x5,-1)
+        bitfont.draw_text(str(cycle_seconds),32,27,font3x5,-1)
     # STATUS TEXT
-    draw_text(text.upper(),0,20,font3x5)
+    bitfont.draw_text(text.upper(),0,20,font3x5)
 
     cu.update(graphics)
 
